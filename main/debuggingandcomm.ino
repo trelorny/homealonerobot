09e0a89540cef3207305c8a08037251045b17c11
Tim Dauer <tim.dauer@dauernet.de>
2017-03-06 18:37:35 +0100

    Added Bluetooth module

--- main/debuggingandcomm.ino
+++ main/debuggingandcomm.ino
@@ -1,3 +1,25 @@
+#include <SoftwareSerial.h>
+
+#define rxPin A2
+#define txPin A3
+
+SoftwareSerial btSerial(rxPin, txPin);
+String btData;
+
+void initComm(){
+  btSerial.begin(9600);
+  btSerial.println("bluetooth available");
+}
+
+void fetchCommand() {
+    if (btSerial.available()){
+      btData = btSerial.readString();
+      printToSerial("Got message: " + btData);
+    }
+}
+
+
 void printToSerial(String msg) {
   Serial.println(millis() + msg);
+  btSerial.println(millis() + msg);
 }
--- main/main.ino
+++ main/main.ino
@@ -27,6 +27,8 @@ void loop() {
   }
 
   holdCourse(BEARING);
+  delay(200);
+  fetchCommand();
 }
 
 
